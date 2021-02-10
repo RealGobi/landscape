dcbadcf7637236424f0e5a31faadf8e81f99cb34
Jimmy Svensson <jimmy888swe@gmail.com>
2021-02-11 00:12:11 +0100

    feat: fetch data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
+import React, { useEffect, useState } from 'react';
+import Population from './views/Population';
+import axios from 'axios';
+import { sqlQuery } from './functions/SqlQuery';
+
 
 function App() {
+  const [query, setQuery] = useState([]);
+
+  useEffect(() => {
+    axios.post('/OV0104/v1/doris/sv/ssd/START/BE/BE0101/BE0101A/FolkmangdDistrikt', sqlQuery)
+    .then(res => {
+      setQuery(res.data.data);
+    })
+  }, []);
+
+
+
   return (
     <div className="App">
-      App
+      <Population query={query} />
     </div>
   );
 }
