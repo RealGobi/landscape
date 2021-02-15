bfbfebd8d3538845b0fd83b5f9ee18ecc1b26aa7
Jimmy Svensson <jimmy888swe@gmail.com>
2021-02-15 01:21:05 +0100

    feat: added switch

--- src/component/Mytable.js
+++ src/component/Mytable.js
@@ -1,32 +1,45 @@
-import React from 'react'
+import React, {useState} from 'react';
+import Switch from '@material-ui/core/Switch';
+
 
 const Mytable = (props) => {
+  const [checked, setChecked] = useState(false);
 
+  const handleChange = (e) => {
+    setChecked(!checked);
+  };
   const { sortDuplicateHeader, result } = props;
 
   console.log(result);
   return (
-    <table className="styled-table">
-    <thead>
-        <tr>
-      {sortDuplicateHeader.map((header, idx) => {
-       return <th key={idx}>{ header }</th>
-      })}
-        </tr>
-    </thead>
-    <tbody>
-          {result.map((res, idx) => {
-            return(
-              <tr key={idx} className="alignLeft">
-                <td>{ res.title }</td>
-                <td>{ res.data[0].pop }</td>
-                <td>{ res.data[1].pop }</td>
-                <td>{ res.data[2].pop }</td>
-              </tr>
-            )
+    <>
+      <Switch
+        checked={checked && checked}
+        onChange={handleChange}
+        name="checked"
+      />
+      <table className="styled-table">
+      <thead>
+          <tr>
+        {sortDuplicateHeader.map((header, idx) => {
+        return <th key={idx}>{ header }</th>
         })}
-    </tbody>
-</table>
+          </tr>
+      </thead>
+      <tbody>
+            {result.map((res, idx) => {
+              return(
+                <tr key={idx} className="alignLeft">
+                  <td>{ res.title }</td>
+                  <td>{ res.data[0].pop }</td>
+                  <td>{ res.data[1].pop }</td>
+                  <td>{ res.data[2].pop }</td>
+                </tr>
+              )
+          })}
+      </tbody>
+  </table>
+</>
   )
 }
 
